a5ae489f067ab45de781989147d17a389fa55a8e
Aturaidowu <aturaidowu@yahoo.com>
2023-08-15 01:37:54 -0700

    Task1_print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print alphabets in lower case
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+		_putchar(j);
+	_putchar('\n');
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN _H
 int _putchar(char c);
+void print_alphabet(void);
+
 #endif
 
