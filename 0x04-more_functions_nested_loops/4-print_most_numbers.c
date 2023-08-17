6f43d910afa93a020a110628750253862b07f3eb
Aturaidowu <aturaidowu@yahoo.com>
2023-08-17 13:00:11 -0700

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 to 9 excluding 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		if (k != 2 && k != 4)
+			_putchar(k + '0');
+	}
+	_putchar('\n');
+}
