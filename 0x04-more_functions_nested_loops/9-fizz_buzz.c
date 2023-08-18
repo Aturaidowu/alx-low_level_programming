dd30bed1dff97db2fa8981a9941138565e1667d4
Aturaidowu <aturaidowu@yahoo.com>
2023-08-18 04:34:40 -0700

    Task8 FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s", "FizzBuss");
+			printf("%s", "FizzBuzz");
 		else if (i % 3 == 0)
 			printf("%s", "Fizz");
 		else if (i % 5 == 0)
