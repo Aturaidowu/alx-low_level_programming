0cc7ffd8325207ae3e619880c895699a6cc17c3a
Aturaidowu <aturaidowu@yahoo.com>
2023-09-01 07:25:29 -0700

    Minimal number of coins for change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,42 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - prints minimum number of coins to make a change
- *
- * @argc: count argument
- *
- * @argv: vector argument
- *
- * Return: 0
+ * main - print the minimum number of coin to make
+ * a change for an amount
+ * @argc: number of command line arg
+ * @argv: array that holds the command line arg...
+ * Return: (0) when successful
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int cents;
-	int coins = 0;
+	int cents, mncoin = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	cents = atoi(argv[1]);
-	if (cents < 0)
-		printf("%d\n", 0);
+
 	while (cents > 0)
 	{
 		if (cents >= 25)
-			cents = cents - 25;
+			cents -= 25;
 		else if (cents >= 10)
-			cents = cents - 10;
+			cents -= 10;
 		else if (cents >= 5)
-			cents = cents - 5;
+			cents -= 5;
 		else if (cents >= 2)
-			cents = cents - 2;
+			cents -= 2;
 		else if (cents >= 1)
-			cents = cents - 1;
-		coins++;
+			cents -= 1;
+		mncoin += 1;
 	}
-	printf("%d\n", coins);
+	printf("%d\n", mncoin);
 	return (0);
 }
